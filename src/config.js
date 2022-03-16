def2e473f214d6187ba5028f26a36ed10808b53c
Cleberson Saller <contato@csaller.com>
2022-03-15 21:06:28 -0300

    fix: changed doteninit method to allow production run

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv'
-import path from 'path'
-
-dotenv.config({
-  path: path.resolve(__dirname, '../.env')
-})
+if (process.env.NODE_ENV === 'develop') {
+  require('dotenv').config()
+}
 
 export const NODE_ENV = process.env.NODE_ENV || 'develop'
 export const PORT = process.env.PORT || 3000
