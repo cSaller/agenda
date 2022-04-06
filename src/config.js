bc217bc4741edb659626e9a935f36e0665f5f864
Cleberson Saller <cleberson600@gmail.com>
2022-04-06 20:01:29 -0300

    fix: changed default variable operator from OR to nullish coalescing

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === 'develop') {
   require('dotenv').config()
 }
 
-export const NODE_ENV = process.env.NODE_ENV || 'develop'
-export const PORT = process.env.PORT || 3000
-export const ACCESS_SECRET = process.env.ACCESS_SECRET || 'mysupersecret'
-export const ACCESS_EXPIRES = process.env.ACCESS_EXPIRES || '1d'
+export const NODE_ENV = process.env.NODE_ENV ?? 'develop'
+export const PORT = process.env.PORT ?? 3000
+export const ACCESS_SECRET = process.env.ACCESS_SECRET ?? 'mysupersecret'
+export const ACCESS_EXPIRES = process.env.ACCESS_EXPIRES ?? '1d'
